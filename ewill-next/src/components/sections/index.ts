03d92e69e934cdbb40d8fdc51bf274ec4de7ef07
ericcai0814 <d4070707@gmail.com>
2026-01-08 15:02:51 +0800

    chore: 更新 .gitignore 並移除 dist/ 追蹤
    - 取消 dist/ 註解，正式忽略
    - 新增 static-app/, next-app/, nuxt-app/ 忽略
    - 移除 dist/ 目錄的 Git 追蹤

--- ewill-next/src/components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { HeroSection } from './HeroSection'
-export { AboutSection } from './AboutSection'
-export { ServicesSection } from './ServicesSection'
-export { SolutionsSection } from './SolutionsSection'
-
--- ewill-next/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button'
-export { Card } from './Card'
-export { ResponsiveImage } from './ResponsiveImage'
-export { PopupBanner } from './PopupBanner'
-
